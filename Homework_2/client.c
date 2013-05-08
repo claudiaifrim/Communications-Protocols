849d1c52effe1fcc3cab16a3a77bf2668c40e69c
flaviusone <flaviusone@yahoo.com>
2013-05-09 01:25:51 +0300

    Adaugat schelet pentru main files

---
+++ Homework_2/client.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+
+#define BUFLEN 256
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
